6c5aab03dcce0812c93ea6a6218d95fd23b73b0a
Alyssa Grigg <algrigg3@comcast.net>
2025-04-21 16:18:04 -0400

    had to fix the port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import recipeRoutes from '../server/routes/Recipes.js';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
